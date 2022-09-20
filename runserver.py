9665f4bd38d0c071329ce1fe303c4420ddfaf494
ljnath <lakhya.sci@gmail.com>
2022-09-20 23:40:06 +0530

    added support to use default config file when no config file is specified

--- runserver.py
+++ runserver.py
@@ -1,5 +1,6 @@
 
 import argparse
+import os
 import sys
 
 from getDomainAge import GetDomainAge, app
@@ -9,11 +10,13 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--config', nargs=1, help='Complete path to application config file')
     args = parser.parse_args()
 
-    if not args.config:
+    DEFAULT_CONFIG_FILE = 'config.json'
+    
+    if not args.config and not os.path.exists(DEFAULT_CONFIG_FILE):
         parser.print_help()
         sys.exit(1)
 
-    config_file = args.config[0]
+    config_file = args.config[0] if args.config else DEFAULT_CONFIG_FILE
 
     get_domain_age = GetDomainAge(config_file)
     get_domain_age.run()
