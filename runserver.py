3dc2cdaa15f1ee7d30df8f027c48407313bc2036
ljnath <lakhya.sci@gmail.com>
2022-09-21 01:50:20 +0530

    restored changes

--- runserver.py
+++ runserver.py
@@ -10,13 +10,11 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--config', nargs=1, help='Complete path to application config file')
     args = parser.parse_args()
 
-    DEFAULT_CONFIG_FILE = 'config.json'
-    
-    if not args.config and not os.path.exists(DEFAULT_CONFIG_FILE):
+    if not args.config:
         parser.print_help()
         sys.exit(1)
 
-    config_file = args.config[0] if args.config else DEFAULT_CONFIG_FILE
+    config_file = args.config[0]
 
     get_domain_age = GetDomainAge(config_file)
     get_domain_age.run()
